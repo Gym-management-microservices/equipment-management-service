3a7dcb873d8c2219ddac8de6d9e696c0676afc52
Pabl0FPP <pinedapablo6718@gmail.com>
2026-02-18 22:50:25 -0500

    feat: add base project

---
+++ src/main/java/micro/gym/equipmentmanagementservice/EquipmentManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package micro.gym.equipmentmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipmentManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EquipmentManagementServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/micro/gym/equipmentmanagementservice/EquipmentManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package micro.gym.equipmentmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipmentManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
