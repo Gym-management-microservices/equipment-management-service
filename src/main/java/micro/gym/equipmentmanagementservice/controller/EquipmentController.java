fba8baff84eb42e889399db43494ea3bffac4dfa
Pabl0FPP <pinedapablo6718@gmail.com>
2026-03-11 01:33:35 -0500

    feat: add security to endpoints

--- src/main/java/micro/gym/equipmentmanagementservice/controller/EquipmentController.java
+++ src/main/java/micro/gym/equipmentmanagementservice/controller/EquipmentController.java
@@ -3,6 +3,7 @@ package micro.gym.equipmentmanagementservice.controller;
 import micro.gym.equipmentmanagementservice.model.*;
 import micro.gym.equipmentmanagementservice.service.EquipmentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,26 +16,31 @@ public class EquipmentController {
     private EquipmentService equipmentService;
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ADMIN', 'TRAINER')")
     public List<Equipment> getAll() {
         return equipmentService.getAllEquipment();
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'TRAINER')")
     public Equipment getById(@PathVariable String id) {
         return equipmentService.getEquipment(new EquipmentId(id));
     }
 
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public Equipment create(@RequestBody Equipment equipment) {
         return equipmentService.createEquipment(equipment);
     }
 
     @PutMapping("/{id}/status")
+    @PreAuthorize("hasRole('ADMIN')")
     public void changeStatus(@PathVariable String id, @RequestBody EquipmentStatus newStatus) {
         equipmentService.changeStatus(new EquipmentId(id), newStatus);
     }
 
     @PutMapping("/{id}/quantity")
+    @PreAuthorize("hasRole('ADMIN')")
     public void updateQuantity(@PathVariable String id, @RequestBody int quantity) {
         equipmentService.updateQuantity(new EquipmentId(id), quantity);
     }
